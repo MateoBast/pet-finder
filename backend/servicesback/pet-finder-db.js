606893e99b583afbfed4988427342e303fb1ad93
MateoBast <mateobastidas146@gmail.com>
2026-09-14 16:44:16 -0300

    Update pet-finder-db.js

--- backend/servicesback/pet-finder-db.js
+++ backend/servicesback/pet-finder-db.js
@@ -1,12 +1,17 @@
-import pkg from 'pg';
-const { Pool } = pkg;
+import pg from 'pg';
+import dotenv from 'dotenv';
 
+// Cargar las variables de entorno
+dotenv.config();
+
+const { Pool } = pg;
+
+// Crear un pool de conexiones usando la URL de la base de datos
 const pool = new Pool({
-  user: 'postgres', // tu usuario
-  host: 'localhost', // o la dirección IP si está en otro lugar
-  database: 'pet-finder-db', // el nombre de tu base de datos
-  password: 'Timoteo23', // tu contraseña
-  port: 5432, // puerto por defecto
+  connectionString: process.env.DATABASE_URL, // Usar la variable de entorno
+  ssl: {
+    rejectUnauthorized: false, // Esto es necesario para conexiones en producción
+  },
 });
 
 export default pool;
